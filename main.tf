db5373938263d3fb5ceec50f3a40c5f69a6fa23a
Adam K <50672995+kardell2006g@users.noreply.github.com>
2026-09-11 10:40:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,16 @@ data "aws_vpc" "target" {
   id = "vpc-04ab17a47803f2f91"
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.aws_vpc.target.id
+data "aws_internet_gateway" "existing" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [data.aws_vpc.target.id]
+  }
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = data.aws_vpc.target.id
-  cidr_block              = "10.64.0.0/24" # Ensure this does not overlap with existing subnets
+  cidr_block              = "10.64.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-2a"
   tags = {
@@ -25,7 +28,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = data.aws_internet_gateway.existing.id
   }
 
   tags = {
@@ -37,54 +40,3 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.target.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # For production, restrict this!
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_instance" "vault_linux" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
-  key_name                    = "vault"
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "vault-linux-instance"
-  }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.vault_linux.public_ip
-}              
